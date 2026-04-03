b1627468a7a5cde36bbfc04ef9327de65aed94a1
Nikhil-cybe <nikhil7880667470@gmail.com>
2026-04-03 23:58:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
-let input = document.getElementById('inputbox');
-let buttons = document.querySelectorAll('button');
+let inputslider = document.getElementById("inputSlider");
+let sliderValue = document.getElementById("sliderValue");
+let passBox = document.getElementById("passBox");
+let lowercase = document.getElementById("lowercase");
+let uppercase = document.getElementById("uppercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbols");
+let genBtn = document.getElementById("genBtn");
+let copyIcon = document.getElementById("copyIcon");
 
-let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) => {
-        let value=e.target.innerHTML;
+sliderValue.textContent = inputslider.value;
 
-        if (value === '=') {
-            string = eval(string);
-            input.value = string;
-        }
-        else if(e.target.innerHTML=='AC'){
-            string="";
-            input.value=string;
-        }
-        else if(value=='DEL'){
-            string =string.substring(0,string.length-1)
-            input.value=string
-        }
-        else {
-            string += e.target.innerHTML;
-            input.value = string;
-        }
-    });
+inputslider.addEventListener('input', () => {
+  sliderValue.textContent = inputslider.value;
 });
 
+genBtn.addEventListener('click', () => {
+  passBox.value = generatePassword();
+});
+
+let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let allNumbers = "0123456789";
+let allSymbols = "~@#$%^&*";
+
+function generatePassword() {
+  let genpassword = "";
+  let allChars = "";
+
+  if (lowercase.checked) allChars += lowerChars;
+  if (uppercase.checked) allChars += upperChars;
+  if (numbers.checked) allChars += allNumbers;
+  if (symbols.checked) allChars += allSymbols;
+
+  if (allChars.length === 0) {
+    return 'select option';
+  }
+
+  for (let i = 0; i < inputslider.value; i++) {
+    genpassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
+  }
+  return genpassword;
+}
+
+copyIcon.addEventListener('click', () => {
+  if (passBox.value && passBox.value.length > 0) {
+    navigator.clipboard.writeText(passBox.value);
+    copyIcon.innerText = "check";
+    copyIcon.title = "Password Copied";
+    setTimeout(() => {
+      copyIcon.innerText = "content_copy";
+      copyIcon.title = "";
+    }, 2000);
+  }
+});
